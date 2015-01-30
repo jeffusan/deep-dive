d487f1847f89d546ccbbb3882fde042f60a4947c
Jeff Hemminger <jeff@atware.co.jp>
2015-01-30 15:57:41 +0900

    adding tests

--- test/SecuritySpec.scala
+++ test/SecuritySpec.scala
@@ -17,6 +17,7 @@ import play.api.libs.json._
 class UserServiceSpec extends PlaySpec with MockitoSugar {
 
   "User#fromJson" should {
+
     "match emails when converted from json" in {
       val userJson: JsValue = Json.parse("""
       {
@@ -30,18 +31,39 @@ class UserServiceSpec extends PlaySpec with MockitoSugar {
 
       user.email mustBe "tester@json.com"
     }
+
+    "error when cannot be parsed" in {
+      val badUserJson: JsValue = Json.parse("""
+      {
+       "id" : 1,
+       "email" : "tester@json.com",
+       "name" : "Tester"
+      }
+      """)
+
+      try {
+        val user: User = (badUserJson).as[User]
+
+        fail()
+      } catch {
+        case jsr: JsResultException =>
+        case _: Exception => fail()
+      }
+
+    }
   }
+
 
   "User#toJson" should {
     "match emails when converted to json" in {
-      val user: User = new User(Some(1), "tester@json.com", Some("secret"), "tester", "Administrator")
+      val user: User = new User(Some(1), "tester@json.com", null, "tester", "Administrator")
 
       val userJson: JsValue = Json.toJson(user)
 
       val email: String = (userJson \ "email").as[String]
 
       email mustBe "tester@json.com"
-
     }
+
   }
 }
