a3edf4156dac35c3df70f2c5160943ed33e49437
Jeff Hemminger <jeff@atware.co.jp>
2015-02-15 11:01:57 +0900

    tried out reactable, changed background for admin

--- app/assets/js/regions.js
+++ app/assets/js/regions.js
@@ -2,9 +2,33 @@
 /*global React:false */
 /*global auth:false */
 /*global $:false */
-/*global Reactable:false */
 
-var Table = Reactable.Table;
+var CreateRegion = React.createClass({
+  render: function() {
+    return (
+            /* jshint ignore:start */
+<div class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+        </div>
+              /* jshint ignore:end */
+    );
+  }
+});
+
 var Regions = React.createClass({
 
 
@@ -46,9 +70,10 @@ var Regions = React.createClass({
   },
 
   render: function() {
+
     return (
       /* jshint ignore:start */
-      <div id="page-wrapper">
+        <div id="page-wrapper">
         <div className="container-fluid">
           <div className="row">
             <div className="col-lg-12">
@@ -56,7 +81,6 @@ var Regions = React.createClass({
               Regions
               </h1>
               <h3 id='errors'>{this.state.mess}</h3>
-              <Table className="table table-bordered table-hover" data={this.state.data} itemsPerPage={4} sortable={true}/>
             </div>
           </div>
         </div>
