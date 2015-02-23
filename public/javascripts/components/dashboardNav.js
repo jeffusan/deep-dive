638415cf4ded3a152df905602d327803cc67ac50
Jeff Hemminger <jeff@atware.co.jp>
2015-02-23 15:20:01 +0900

    refactor of menu

--- public/javascripts/components/dashboardNav.js
+++ public/javascripts/components/dashboardNav.js
@@ -6,20 +6,20 @@ var DashboardNav = React.createClass({
 
   render: function() {
     return (
-              /* jshint ignore:start */
-           <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-               <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="index.html">Deep Dive!</a>
-            </div>
-           <ul className="nav navbar-right top-nav">
+      /* jshint ignore:start */
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="index.html">Deep Dive!</a>
+       </div>
+        <ul className="nav navbar-right top-nav">
           <li className="dropdown">
-        <a href="#" className="dropdown-toggle" data-toggle="dropdown"><i className="fa fa-user"></i> {auth.getUserName()} <b className="caret"></b></a>
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown"><i className="fa fa-user"></i> {auth.getUserName()} <b className="caret"></b></a>
             <ul className="dropdown-menu">
               <li>
                 <a href="#"><i className="fa fa-fw fa-user"></i> Profile</a>
@@ -37,25 +37,39 @@ var DashboardNav = React.createClass({
             </ul>
            </li>
           </ul>
-           <div className="collapse navbar-collapse navbar-ex1-collapse">
+        <div className="collapse navbar-collapse navbar-ex1-collapse">
           <ul className="nav navbar-nav side-nav">
             <li>
-              <a href="/dashboard"><i className="fa fa-fw fa-dashboard"></i> Dashboard</a>
+              <a href="/dashboard"><i className="fa fa-fw fa-heartbeat"></i> Dashboard</a>
             </li>
             <li>
-              <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i className="fa fa-fw fa-arrows-v"></i> Meta Models <i className="fa fa-fw fa-caret-down"></i></a>
-              <ul id="demo" className="collapse">
-                <li>
-                  <a href="/dashboard/regions"><i className="fa fa-fw fa-table"></i> Regions</a>
-                </li>
-                <li>
-                  <a href="#"><i className="fa fa-fw fa-table"></i> SubRegions</a>
-                </li>
-                <li>
-                  <a href="#"><i className="fa fa-fw fa-table"></i> Sites</a>
-                </li>
-          </ul>
-
+              <a href="/dashboard"><i className="fa fa-fw fa-database"></i> Regions</a>
+            </li>
+            <li>
+              <a href="/dashboard"><i className="fa fa-fw fa-database"></i> Subregions</a>
+            </li>
+            <li>
+              <a href="/dashboard"><i className="fa fa-fw fa-database"></i> Sites</a>
+            </li>
+            <li>
+              <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i className="fa fa-fw fa-arrows-v"></i> Benthic <i className="fa fa-fw fa-caret-down"></i></a>
+                <ul id="demo" className="collapse">
+                  <li>
+                    <a href="/dashboard/regions"><i className="fa fa-fw fa-upload"></i> Upload Benthic Data</a>
+                  </li>
+                  <li>
+                    <a href="/dashboard/regions"><i className="fa fa-fw fa-table"></i> Reef Types</a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fa fa-fw fa-table"></i> Survey Events</a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fa fa-fw fa-table"></i> Categories</a>
+                  </li>
+                  <li>
+                    <a href="#"><i className="fa fa-fw fa-table"></i> Sub Categories</a>
+                  </li>
+                </ul>
             </li>
           </ul>
         </div>
